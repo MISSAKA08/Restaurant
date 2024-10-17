6a4287b71936b1dd3e4e2bb8d339f060d6cc9893
Supun200110 <supunnishantha45@gmail.com>
2024-10-17 15:09:44 +0530

    comm

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(0);
 session_start(); 
 
 ?>
-<head>
+<head> //comment
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">   
@@ -17,7 +17,6 @@ session_start();
     <link href="css/animsition.min.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet"> 
-    
     
  
 </head>
